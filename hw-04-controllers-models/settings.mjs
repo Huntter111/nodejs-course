02f9b3dea495403623a7679f008288ebe06649c0
Hunter111 <sukhomudrenkov17@gmail.com>
2026-01-13 20:06:17 +0200

    Update styles and views in hw-04 controllers and models

--- hw-04-controllers-models/settings.mjs
+++ hw-04-controllers-models/settings.mjs
@@ -1,9 +1,9 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
+import path from 'path' // path — для роботи з файловими шляхами
+import { fileURLToPath } from 'url' // fileURLToPath — щоб перетворити import.meta.url у звичайний шлях
 
-const __filename = fileURLToPath(import.meta.url) // get the resolved path to the file
-const __dirname = path.dirname(__filename) // get the name of the directory
-const filePath = path.join(__dirname, '/data/booksData.json')
+const __filename = fileURLToPath(import.meta.url) // Отримуємо абсолютний шлях до файлу settings.mjs.
+const __dirname = path.dirname(__filename) // Отримуємо папку, в якій лежить settings.mjs
+const filePath = path.join(__dirname, '/data/booksData.json') // __dirname — папка проєкту, /data/booksData.json — відносний шлях, разом отримуємо /.../hw-04-controllers-models/data/booksData.json
 
 export default {
 	dataPath: filePath,
